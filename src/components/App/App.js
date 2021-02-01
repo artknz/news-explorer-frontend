56f1edd3c81313e040b8b5c1bcebdc8aa91847cf
Artyom Knyazev <a.knyazev7@yandex.ru>
2021-02-01 23:46:32 +0300

    feat: add components for searching

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
-import Header from '../Header/Header';
-
+// import Header from '../Header/Header';
+import SearchForm from '../SearchForm/SearchForm';
 const App = () => {
 
   return (
     <div className="App">
-      <Header />
-
+      {/* <Header /> */}
+      <SearchForm />
     </div>
   );
 }
