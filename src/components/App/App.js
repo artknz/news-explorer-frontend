2abc70085379caab0e2555dabfff948e9783df9b
Artyom Knyazev <a.knyazev7@yandex.ru>
2021-02-03 22:17:02 +0300

    feat: add cards and preloader components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
-// import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
+import Main from '../Main/Main';
+
 const App = () => {
 
   return (
     <div className="App">
-      {/* <Header /> */}
       <SearchForm />
+      <Main />
     </div>
   );
 }
