b25eebadbea47a680cd0caa3a4ea92ca4c81e71a
Artyom Knyazev <a.knyazev7@yandex.ru>
2021-02-12 23:26:26 +0300

    hotfix: optimize tab resolution, fix validation

--- src/components/PopupRegister/PopupRegister.js
+++ src/components/PopupRegister/PopupRegister.js
@@ -21,8 +21,8 @@ export default function PopupRegister({name, isOpen, onClose}) {
       <span id="link-input-error" className="popup__field-error" />
 
       <h4 className="popup__field-name">Имя</h4>
-      <input id="link-input" type="url" value={''} onChange={''} placeholder="Введите свое имя" name="link" className="popup__field popup__field_type_link" required />
-      <span id="link-input-error" className="popup__field-error" />
+      <input id="name-input" type="url" value={''} onChange={''} placeholder="Введите свое имя" name="link" className="popup__field popup__field_type_link" required />
+      <span id="name-input-error" className="popup__field-error" />
     </PopupWithForm>
   );
 };
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -12,7 +12,7 @@ export default function SearchForm({onAuthClick}) {
         <h1 className="search-form__title">Что творится в мире?</h1>
         <p className="serach-form__caption">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
         <form className="search-form__bar">
-          <input type="search" placeholder="Введите тему новости" className="search-form__type-line"></input>
+          <input type="search" placeholder="Введите тему новости" className="search-form__type-line" required></input>
           <button type="submit" className="search-form__submit-button">Искать</button>
         </form>
       </div>
